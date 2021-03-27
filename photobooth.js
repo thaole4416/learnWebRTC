1c9d956b8942d93c2438915fa5465e8fec1150ab
Thao <thaole4416@gmail.com>
2021-03-27 21:39:00 +0700

    add filter to video

--- photobooth.js
+++ photobooth.js
@@ -7,16 +7,35 @@ function hasUserMedia() {
   );
 }
 
+function addFilterToVideo() {
+  const filters = ["", "grayscale", "sepia", "invert"];
+  let currentFilter = 0;
+  const canvas = document.querySelector("canvas");
+  const video = document.querySelector("video");
+  video.addEventListener("click", function (event) {
+    console.log(streaming);
+    if (streaming) {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const context = canvas.getContext("2d");
+      context.drawImage(video, 0, 0);
+      currentFilter++;
+      if (currentFilter > filters.length - 1) currentFilter = 0;
+      canvas.className = filters[currentFilter];
+    }
+  });
+}
+
 function main() {
+  const video = document.querySelector("video");
+  const canvas = document.querySelector("canvas");
+  let streaming = false;
   if (hasUserMedia()) {
     navigator.getUserMedia =
       navigator.getUserMedia ||
       navigator.webkitGetUserMedia ||
       navigator.mozGetUserMeida ||
       navigator.msGetUserMedia;
-    const video = document.querySelector("video");
-    const canvas = document.querySelector("canvas");
-    let streaming = false;
     navigator.getUserMedia(
       { video: true, audio: false },
       function (stream) {
@@ -40,6 +59,7 @@ function main() {
   } else {
     alert("Sorry, your browser does not support getUserMedia.");
   }
+  addFilterToVideo();
 }
 
 main();
