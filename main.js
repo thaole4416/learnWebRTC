f83e11903ac01abf8759593bfd513f9f453c5a84
Thao <thaole4416@gmail.com>
2021-03-27 12:32:47 +0700

    handling multiple devices

--- main.js
+++ main.js
@@ -7,6 +7,39 @@ function hasUserMedia() {
   );
 }
 
+function handleMultipleDevices() {
+  navigator.mediaDevices.enumerateDevices().then(function (sources) {
+    var audioSource = null;
+    var videoSource = null;
+    for (var i = 0; i < sources.length; ++i) {
+      var source = sources[i];
+      if (source.kind === "audio") {
+        console.log("Microphone found:", source.label, source.id);
+        audioSource = source.id;
+      } else if (source.kind === "video") {
+        console.log("Camera found:", source.label, source.id);
+        videoSource = source.id;
+      } else {
+        console.log("Unknown source found:", source);
+      }
+    }
+    var constraints = {
+      audio: { optional: [{ sourceId: audioSource }] },
+      video: { optional: [{ sourceId: videoSource }] },
+    };
+    navigator.getUserMedia(
+      constraints,
+      function (stream) {
+        var video = document.querySelector("video");
+        video.src = window.URL.createObjectURL(stream);
+      },
+      function (error) {
+        console.log("Raised an error when capturing:", error);
+      }
+    );
+  });
+}
+
 function main() {
   if (hasUserMedia()) {
     navigator.getUserMedia =
@@ -53,4 +86,4 @@ function main() {
   }
 }
 
-main();
+handleMultipleDevices();
