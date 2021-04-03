61daf6c4b21334cf90d1f05ca6d69a51cb0110d7
Thao <thaole4416@gmail.com>
2021-04-03 09:15:40 +0700

    signaling server: user login

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const WebSocketServer = require("ws").Server,
-  wss = new WebSocketServer({ port: 8888 });
+  wss = new WebSocketServer({ port: 8888 }),
+  users = {};
+
+function sendTo(conn, message) {
+  conn.send(JSON.stringify(message));
+}
 
 wss.on("connection", function (conn) {
   console.log("user connected");
 
   conn.on("message", function (message) {
-    console.log("Get message", message);
+    try {
+      data = JSON.parse(message);
+    } catch (err) {
+      console.log(err);
+      data = {};
+    }
+    switch (data.type) {
+      case "login": {
+        console.log(`user logged in as ${data.name}`);
+        if (users[data.name]) {
+          sendTo(conn, {
+            type: "login",
+            success: false,
+          });
+        } else {
+          users[data.name] = conn;
+          conn.name = data.name;
+          sendTo(conn, {
+            type: "login",
+            success: true,
+          });
+        }
+        break;
+      }
+      default: {
+        sendTo(conn, {
+          type: "error",
+          message: "unrecognized command " + data.type,
+        });
+        break;
+      }
+    }
   });
-
-  conn.send("Hello world");
 });
